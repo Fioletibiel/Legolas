addecc7ef15006695cb866105c82669053491885
Fioletibiel <pawel89kaminski@gmail.com>
2018-06-08 12:09:49 +0200

    Zrobione menu okna, oraz wczytywanie i zapis do pliku :)))

--- venv/OdczytZapis.py
+++ venv/OdczytZapis.py
@@ -1,24 +1,95 @@
 from tkinter import *
+from tkinter import filedialog
 
 class OdczytZapis(object):
 
     def __init__(self):
         self.root = Tk()
 
-    def odczyt(self):
-        # plik = open('nazwa_pliku')
-        # try:
-        #     tekst = plik.read()
-        # finally:
-        #     plik.close()
-        # print(tekst)
-        self.root.nazwa_pliku = filedialog.askopenfilename(title='wybierz plik .txt z zakodowanym grafem, lub z kodem Prüfera',filetypes=("dokument tekstowy","*.txt"))
-        return self.root.nazwa_pliku
+    def odczytPrufer():
+        plik_Prufer = filedialog.askopenfilename(
+            filetypes=(("Text Documents", "*.txt"), ("All files", "*.*")),
+            title="Proszę wybierz plik .txt z kodem Prüfera")
+        if plik_Prufer:
+            try:
+                print("""here it comes: self.settings["template"].set(fname)""")
+            except:  # <- naked except is a bad idea
+                showerror("Open Source File", "Failed to read file\n'%s'" % plik_Prufer)
+            return
+        return plik_Prufer
 
-    def zapis(self):
-        # plik = open('plik', 'w')
-        # plik.write("tresc")
-        # plik.close()
-        self.root.nazwa_pliku = filedialog.asksavefilename(title='wybierz plik .txt, do którego chcesz zapisać zakodowany graf, lub kod Prüfera',filetypes=("dokument tekstowy","*.txt"))
-        return self.root.nazwa_pliku
-        # x = input('Podaj x: ')
+    def odczytDOT():
+        plik_DOT = filedialog.askopenfilename(
+            filetypes=(("Text Documents", "*.txt"), ("All files", "*.*")), title="Proszę wybierz plik .txt z kodem DOT")
+        if plik_DOT:
+            try:
+                print("""here it comes: self.settings["template"].set(fname)""")
+            except:  # <- naked except is a bad idea
+                showerror("Open Source File", "Failed to read file\n'%s'" % plik_DOT)
+            return
+        return plik_DOT
+
+    def zapisPrufer():
+        plik_Prufer = filedialog.asksaveasfilename(
+            filetypes=(("Text Documents", "*.txt"), ("All files", "*.*")),
+            title='Proszę wybierz plik .txt, do którego chcesz zapisać kod Prüfera namalowanego grafu.',
+            defaultextension='.txt')
+        if plik_Prufer:
+            try:
+                print("""here it comes: self.settings["template"].set(fname)""")
+            except:  # <- naked except is a bad idea
+                showerror("Open Source File", "Failed to read file\n'%s'" % plik_Prufer)
+            return
+        return plik_Prufer
+        plik = open(plik_Prufer, 'w')
+        plik.write("tresc")
+        plik.close()
+
+    def zapisDOT():
+        plik_DOT = filedialog.asksaveasfilename(filetypes=(("Text Documents", "*.txt"), ("All files", "*.*")),
+                                                        title='Proszę wybierz plik .txt, do którego chcesz zapisać kod DOT namalowanego grafu.',
+                                                        defaultextension='.txt')
+        if plik_DOT:
+            try:
+                print("""here it comes: self.settings["template"].set(fname)""")
+            except:  # <- naked except is a bad idea
+                showerror("Open Source File", "Failed to read file\n'%s'" % plik_DOT)
+            return
+        return plik_DOT
+        plik = open(plik_DOT, 'w')
+        plik.write("tresc")
+        plik.close()
+
+
+
+
+
+    # def odczyt(self):
+    #     # plik = open('nazwa_pliku')
+    #     # try:
+    #     #     tekst = plik.read()
+    #     # finally:
+    #     #     plik.close()
+    #     # print(tekst)
+    #
+    #     # self.root.nazwa_pliku = filedialog.askopenfilename(title='wybierz plik .txt z zakodowanym grafem, lub z kodem Prüfera',filetypes=("Text Documents","*.txt"))
+    #     # return self.root.nazwa_pliku
+    #
+    #     # self.root.nazwa_pliku = filedialog.askopenfilenames(parent=self)
+    #     # self.title_count.set('{{file(s)'.format(len(self.selected_files)))
+    #
+    #     self.root.nazwa_pliku = filedialog.askopenfilename(filetypes=(("Text Documents", "*.txt"),("All files", "*.*")),title = "Wybierz plik .txt z kode")
+    #     if fname:
+    #         try:
+    #             print("""here it comes: self.settings["template"].set(fname)""")
+    #         except:                     # <- naked except is a bad idea
+    #             showerror("Open Source File", "Failed to read file\n'%s'" % fname)
+    #         return
+    #
+    # def zapis(self):
+    #     # plik = open('plik', 'w')
+    #     # plik.write("tresc")
+    #     # plik.close()
+    #     self.root.nazwa_pliku = filedialog.asksavefilename(title='wybierz plik .txt, do którego chcesz zapisać zakodowany graf, lub kod Prüfera',filetypes=("Text Documents","*.txt"))
+    #     return self.root.nazwa_pliku
+    #     # x = input('Podaj x: ')
--- venv/TkinterGUI.py
+++ venv/TkinterGUI.py
@@ -1,101 +1,29 @@
 from tkinter import *
 import tkinter
+from OdczytZapis import OdczytZapis
+
 
 class TkinterGUI(object):
 
-    # def __init__(self):
-    #     self.root = Tk()
-    #     self.root.geometry("200x250")
-    #     self.root.title('Legolas')
-    #     self.root.iconbitmap('celtic_tree.ico')
-    #
-    #     l1 = Label(self.root, text = "Napis_1").grid(row=0).pack(side=TOP)
-    #     l2 = Label(self.root, text = "Napis_2").grid(row=1).pack(side=TOP)
-    #
-    #     ent = Entry(self.root).grid(row=0, column=1).pack(side=TOP)
-    #     var_chk = IntVar()
-    #     rd1 = Radiobutton(self.root, text = "Radiobutton_1", variable = var_chk, value=1).grid(row=1, column=1, sticky=W).pack(side=TOP)
-    #     rd2 = Radiobutton(self.root, text = "Radiobutton_2", variable = var_chk, value=2).grid(row=1, column=1, sticky=E).pack(side=TOP)
-    #     btn = Button(self.root, text="Przycisk", bg="black", fg="blue").grid(row=2, columnspan=2, command = show_data).pack(side=TOP)
-    #     txt = Text(self.root, width=250, height=10, wrap=WORD).grid(row=3, columnspan=2, sticky=W).pack(side=TOP)
-    #
-    #     self.root.mainloop()
-    #
-    # def show_data(self):
-    #     txt.delete(0.0, 'end')
-    #     txtName = ent.get()
-    #     gender = var_chk.get()
-    #
-    #     if gender ==1:
-    #         gender = "sir"
-    #     else:
-    #         gender = "ma'm"
-    #
-    #     sentence = "Hello, " + str(txtName) + "\nHow are you " + gender + "?"
-    #     txt.insert(0.0, sentence)
-
-
     def __init__(self):
         self.root = Tk()
-        # self.root.geometry("200x250")
-        self.root.title('Legolas')
+        self.root.geometry("200x250")
+        self.root.title('GIZ projekt')
         self.root.iconbitmap('celtic_tree.ico')
 
 
-        # topFrame = Frame(self.root).pack()
-        # bottomFrame = Frame(self.root).pack(side=BOTTOM)
-        #
-        # button1 = Button(topFrame, text="Button1", fg="red").pack(side=TOP)
-        # button2 = Button(topFrame, text="Button2", fg="blue").pack(side=TOP)
-        # button3 = Button(bottomFrame, text="Button3", fg="green").pack(side=TOP)
-        # button4 = Button(bottomFrame, text="Button4", fg="purple").pack(side=TOP)
-        #
-        # l1 = Label(self.root, text="Napis_1", bg="red", fg="white").pack()
-        # l2 = Label(self.root, text="Napis_2", bg="black", fg="green").pack(fill=X)
-        # l3 = Label(self.root, text="Napis_3", bg="grey", fg="blue").pack(side=LEFT, fill=Y)
 
-
-        # l1 = Label(self.root, text="Name", bg="red", fg="white").grid(row=0, sticky=E)
-        # l2 = Label(self.root, text="Password", bg="black", fg="green").grid(row=1, sticky=E)
-        # l3 = Label(self.root, text="Napis_3", bg="grey", fg="blue").grid(row=2, sticky=W)
-        #
-        # entry1 = Entry(self.root).grid(row=0, column = 1)
-        # entry2 = Entry(self.root).grid(row=1, column = 1)
-        #
-        # c = Checkbutton(self.root, text="text").grid(columnspan=2)
-
-
-        # def printName():
-        #     print("Hello my name is Paul!")
-        # btn1=Button(self.root, text="Print my name", command=printName).pack()
-
-
-        # def printName(event):
-        #     print("Hello my name is Paul!")
-        # btn1 = Button(self.root, text="Print my name")
-        # btn1.bind("<Button-1>", printName)         # <Button-1> oznacza lewy przycisk myszy
-        # btn1.pack()
-
-
-        # def leftClick(event):
-        #     print("Left")
-        #
-        # def middleClick(event):
-        #     print("Middle")
-        #
-        # def rightClick(event):
-        #     print("Right")
-        #
-        # frame = Frame(self.root, width=300, height=250)
-        # frame.bind("<Button-1>", leftClick)
-        # frame.bind("<Button-2>", middleClick)
-        # frame.bind("<Button-3>", rightClick)
-        # frame.pack()
-
-
-
+        menu = Menu(self.root)
+        self.root.config(menu=menu)
+        subMenu1 = Menu(menu)
+        menu.add_cascade(label="Kod Prüfera", menu=subMenu1)
+        subMenu1.add_command(label="Prüfer->Graf", command=OdczytZapis.odczytPrufer)
+        subMenu1.add_command(label="Graf->Prüfer", command=OdczytZapis.zapisPrufer)
+        subMenu2 = Menu(menu)
+        menu.add_cascade(label="Kod DOT", menu=subMenu2)
+        subMenu2.add_command(label="DOT->Graf", command=OdczytZapis.odczytDOT)
+        subMenu2.add_command(label="Graf->DOT", command=OdczytZapis.zapisDOT)
 
 
 
         self.root.mainloop()
-
